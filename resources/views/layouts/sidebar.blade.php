e5b64feece4d4b37c23d0690d87cbd2581b83f50
Doal05 <berandalmalam446@gmail.com>
2024-01-25 22:27:42 +0700

    active nav

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -20,7 +20,7 @@
                     <div class="navbar-nav w-100">
                         <a href="{{ route('dashboard') }}"
                             class="nav-item nav-link {{ Request::is('/') ? 'active' : '' }}"><i
-                                class="fa fa-tachometer-alt me-2"></i>SEHAN</a>
+                                class="fa fa-tachometer-alt me-2"></i>Dashboard</a>
 
 
                         <a href="{{ route('produk.index') }}"
