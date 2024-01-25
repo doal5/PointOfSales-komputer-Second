8308ae1535af9dcfd764c3493ba70ffb534d35a4
Doal05 <berandalmalam446@gmail.com>
2024-01-25 12:14:07 +0700

    aldo

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -60,6 +60,13 @@
                     </a>
                 </nav>
                 <!-- Navbar End -->
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb" class="breadcrumb justify-content-center">
+                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                        <li class="breadcrumb-item"><a href="#">Library</a></li>
+                        <li class="breadcrumb-item active" aria-current="page">Data</li>
+                    </ol>
+                </nav>
 
                 @yield('content')
 
