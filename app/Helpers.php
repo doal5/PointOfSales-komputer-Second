61aae56015f9b350988d8423bf14e0ffd3483657
Doal05 <berandalmalam446@gmail.com>
2024-02-02 21:41:40 +0700

    helpers rupiah

--- app/Helpers.php
+++ app/Helpers.php
@@ -1,8 +1,5 @@
 <?php
 function rupiah($nominal)
 {
-    // if ($prefix) {
-    //     return "Rp." . $prefix . "" . number_format($nominal, 0, ',', '.');
-    // }
     return "Rp." . number_format($nominal, 0, ',', '.');
 }
