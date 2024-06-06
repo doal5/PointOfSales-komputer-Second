6c7a64a6995db686577333062b017de25fea7f4e
Aldo <berandalmalam446@gmail.com>
2024-06-06 21:53:08 +0700

    box dashboard

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -6,7 +6,7 @@
            <div class="row g-3">
                <div class="col-sm-6 col-xl-4">
                    <div class="bg-light rounded d-flex align-items-center justify-content-between p-4">
-                       <i class="fa fa-chart-line fa-3x text-primary"></i>
+                       <i class="fa-solid fa-laptop fa-3x text-primary"></i>
                        <div class="ms-3">
                            <p class="mb-2">Total Produk</p>
                            <h6 class="mb-0">{{ $totalProduk }}</h6>
@@ -15,7 +15,7 @@
                </div>
                <div class="col-sm-6 col-xl-4">
                    <div class="bg-light rounded d-flex align-items-center justify-content-between p-4">
-                       <i class="fa fa-chart-bar fa-3x text-primary"></i>
+                       <i class="fa-solid fa-money-bill fa-3x text-primary"></i>
                        <div class="ms-3">
                            <p class="mb-2">Total Penjualan</p>
                            <h6 class="mb-0">{{ rupiah($totalPenjualan) }}</h6>
